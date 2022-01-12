8670ac95a8613570682aed173f511407ce8fb0c8
Hari Dandapani <haridandapani@gmail.com>
2022-01-11 19:56:10 -0500

    adding working API

--- project.py
+++ project.py
@@ -9,93 +9,128 @@ import random
 
 app = Flask(__name__,static_folder='./static')
 
-apikey = "59jIWfBWdD426XjAdRAjWaF9u5WJTmgm4tzPd3Ik"
+auths = open("auth/legal_auth_tokens.txt", "r")
+legalauthcodes = auths.read().splitlines()
+keys = open("auth/legal_keys.txt", "r")
+legalkeys = keys.read().splitlines()
 
+noauth = app.response_class(response = json.dumps({"message": "Did not get valid auth code in request"}), status = 400, mimetype='application/json')
 nokey = app.response_class(response = json.dumps({"message": "Did not get valid key code in request"}), status = 400, mimetype='application/json')
 
-horoscopes = {'Tim': 'Sagittarius', 'Hari': 'Virgo', 'Eva': 'Sagittarius', 'Colton': 'Aries', 'Galen': 'Aries', 'Anika': 'Libra', 'Siddharth': 'Scorpio'}
+apifailure = app.response_class(response = json.dumps({"message": "Your API call failed due to a server fault"}), status = 400, mimetype='application/json')
 
-@app.route('/introResource', methods = ['GET'])
-def introResource():
-    output = {'message': 'Congrats! You have successfully completed a basic GET request!'}
-    return make200(output)
 
-@app.route('/securedResource', methods = ['GET', 'POST'])
-def securedResource():
+#########################################USERS STUFF#############################################
+
+@app.route('/post-test', methods = ['POST'])
+def post_test():
     try:
         thiskey = request.headers.get("X-Api-Key")
     except:
         return nokey
-    if (thiskey == None or thiskey != apikey):
-        return nokey
-
-    if request.method == 'GET':
-        output = {'message': 'Congrats! You have successfully completed a GET request to a protected resource!'}
-        return make200(output)
-    else:
-        body = json.loads(request.data.decode('utf-8'))
-        try:
-            name = body["name"]
-            output = {'message': 'Congrats, '+name+'! You have successfully completed a POST request to a protected resource!'}
-            return make200(output)
-        except:
-            output = {'message': 'Did not get a value for the name'}
-            return make200(output, 401)
     
-@app.route('/horoscopeResource', methods = ['GET'])
-def horoscopeResource():
-
-    try:
-        thiskey = request.headers.get("X-Api-Key")
-    except:
-        return nokey
-    if (thiskey == None or thiskey != apikey):
-        return nokey
-
-    name = request.args.get('name')
-    
-
-    if (name):
-        try:
-            horoscope = horoscopes[name]
-        except:
-            horoscope = 'None'
-        output = {"message": horoscope}
-        return make200(output)
-
-    else:
-        output = {"message": horoscopes}
-        return make200(output)
-
-@app.route('/addHoroscope', methods = ['POST'])
-def addHoroscope():
-
-    try:
-        thiskey = request.headers.get("X-Api-Key")
-    except:
-        return nokey
-    if (thiskey == None or thiskey != apikey):
-        return nokey
-
     body = json.loads(request.data.decode('utf-8'))
     try:
-        name = body["name"]
-        horoscope = body["horoscope"]
-        if name in horoscopes:
-            horoscopes[name] = horoscopes[name] + " OR " + horoscope
-        else:
-            horoscopes[name] = horoscope
-        output = {'message': 'Added Horoscope!'}
-        return make200(output)
+        auth = body["auth"]
+        return genericJSONLoader(0.0, 0.0, "data/integration.json", auth, thiskey)
     except:
-        output = {'message': 'Did not get a value for the name and/or horoscope'}
-        return make200(output, 401) 
+        return noauth
 
+@app.route('/get-test', methods = ['GET'])
+def get_test():
+    auth = request.args.get('auth')
+    key = request.args.get('key')
+    failure = 0.0
+    removal_odds = 0.0
+    return genericJSONLoader(failure, removal_odds, "data/integration.json", auth, key)
+
+@app.route("/info-one", methods = ['GET'])
+def info_one():
+    auth = request.args.get('auth')
+    key = request.args.get('key')
+    failure_odds = 0.0
+
+    datadict = open("data/studentinfo.json", "r")
+    jsoner = json.loads(datadict.read())
+    dictionary_to_return = jsoner[0:15]
     
+    return dataToRequest(failure_odds, dictionary_to_return, auth, key)
 
-        
+@app.route("/info-two", methods = ['GET'])
+def info_two():
+    auth = request.args.get('auth')
+    key = request.args.get('key')
+    failure_odds = 0.0
+
+    datadict = open("data/studentinfo.json", "r")
+    jsoner = json.loads(datadict.read())
+    dictionary_to_return = jsoner[15:43]
+    
+    return dataToRequest(failure_odds, dictionary_to_return, auth, key)
+
+@app.route("/info-three", methods = ['GET'])
+def info_three():
+    auth = request.args.get('auth')
+    key = request.args.get('key')
+    failure_odds = 0.0
+
+    datadict = open("data/studentinfo.json", "r")
+    jsoner = json.loads(datadict.read())
+    dictionary_to_return = jsoner[43:]
+    
+    return dataToRequest(failure_odds, dictionary_to_return, auth, key)
+
 
 #############################################GENERIC STUFF###########################################
+
+def dataToRequest(failure_odds, dictionary_to_return, auth, key):
+    print(auth)
+
+    # Ensure that auth and key data are legal
+    if not(auth in legalauthcodes):
+        return noauth
+    else:
+        index = legalauthcodes.index(auth)
+        if (legalkeys[index] != key):
+            return nokey
+
+    # Mock a server fault some percent of the time
+    toabort = random.random()
+    if (toabort < failure_odds):
+        return apifailure
+
+    # If you have passed the auth, key, and fault test, then you will receive your data
+    return make200(dictionary_to_return)
+    
+
+    
+    
+
+def genericJSONLoader(failure, removal_odds, file, auth, key):
+    # fails failure percent of the time
+    # removes an element removal_odds percent of the time
+    
+    if not(auth in legalauthcodes):
+        return noauth
+    else:
+        index = legalauthcodes.index(auth)
+        print(index)
+        if (legalkeys[index] != key):
+            return nokey
+    
+    toremove = random.random()
+    toabort = random.random()
+    rent = open(file, "r")
+    jsoner = json.loads(rent.read())
+    
+    if (toremove < removal_odds):
+        numtoremove = 1 + int(random.random() * 2)
+        for _ in range(numtoremove):
+            jsoner.pop(int(random.random() * len(jsoner)))
+    if (toabort < failure):
+        return apifailure
+    
+    return make200(jsoner)
 
 def make200(jsoner, mystatus = 200):
     response = app.response_class(
